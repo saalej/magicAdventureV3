632f849481f8df9a3740cb47c4ef07509102e0bf
Marian Herrera <64176067+saalej@users.noreply.github.com>
2022-11-25 16:36:30 -0600

    Camara

--- Assets/Scrips/Server/ObjectMoveNetworkController.cs
+++ Assets/Scrips/Server/ObjectMoveNetworkController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class ObjectMoveNetworkController : NetworkBehaviour
 {
