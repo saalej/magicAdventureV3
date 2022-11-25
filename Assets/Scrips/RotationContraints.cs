526ea22bb6d378e52b8a7856259c8c0448ae2c05
Marian Herrera <64176067+saalej@users.noreply.github.com>
2022-11-24 20:04:43 -0600

    Camera

--- Assets/Scrips/RotationContraints.cs
+++ Assets/Scrips/RotationContraints.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class RotationContraints : MonoBehaviour
 {
+    private Quaternion contrain;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        contrain = Quaternion.Euler(15,10,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation = contrain;
     }
 }
