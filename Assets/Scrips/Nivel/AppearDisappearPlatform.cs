83f28a82b93a733a71cd859e6a5a88edfe9355d5
Marian Herrera <64176067+saalej@users.noreply.github.com>
2022-11-30 16:01:29 -0600

    Empezar a desaparecer en X segundos de empezar

--- Assets/Scrips/Nivel/AppearDisappearPlatform.cs
+++ Assets/Scrips/Nivel/AppearDisappearPlatform.cs
@@ -4,18 +4,50 @@ using UnityEngine;
 
 public class AppearDisappearPlatform : MonoBehaviour
 {
+    [Header("Time Settings")]
     [SerializeField] private int timeAssign = 5;
     private int timeRandom;
     [SerializeField] private bool isRandom = false;
+
+    [Header("Timeout Setting")]
+    [SerializeField] private bool mustWait = false;
+    [SerializeField] private int timeAwake = 5;
+
+    [Header("GameObject Setting")]
     [SerializeField] GameObject platform;
 
     void Start()
     {
-        if(isRandom == true)
+        if (mustWait)
+        {
+            AppearDisappearAwakeState();
+        } else
+        {
+            AppearDisappearState();
+        }
+    }
+
+    private void AppearDisappearAwakeState()
+    {
+        if (isRandom)
+        {
+            timeRandom = Random.Range(2, 5);
+            InvokeRepeating("ChangeObjectState", timeAwake, timeRandom);
+        }
+        else
+        {
+            InvokeRepeating("ChangeObjectState", timeAwake, timeAssign);
+        }
+    }
+
+    private void AppearDisappearState()
+    {
+        if (isRandom)
         {
             timeRandom = Random.Range(2, 5);
             InvokeRepeating("ChangeObjectState", 0f, timeRandom);
-        } else
+        }
+        else
         {
             InvokeRepeating("ChangeObjectState", 0f, timeAssign);
         }
