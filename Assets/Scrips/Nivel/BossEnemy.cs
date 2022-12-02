dc24c7a99c19aa90b2162bb4de7c44001442de20
Caro :B <102637400+carogatos@users.noreply.github.com>
2022-12-02 00:56:02 -0600

    anim

--- Assets/Scrips/Nivel/BossEnemy.cs
+++ Assets/Scrips/Nivel/BossEnemy.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class BossEnemy : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Animator animatorPlayer;
+    //[SerializeField] private int distance;
 
     void FireRay()
     {
         Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, transform.forward, Color.green);
         RaycastHit hitData;
         Physics.Raycast(ray, out hitData);
-        if (hitData.distance < 5 && hitData.transform.tag == "Player")
+        if (hitData.distance < 9 && hitData.distance > 4 && hitData.transform.tag == "Player")
         {
-            animator.SetBool("isAround", true); 
-            animator.SetTrigger("diz");
+            animator.SetBool("isAround", true);
+            print("isaround");
+            //animator.SetTrigger("diz");
         }
-        else if(hitData.distance < 2 && hitData.transform.tag == "Player")
+        else if(hitData.distance <= 4 && hitData.transform.tag == "Player")
         {
+            print("attack");
             animator.SetBool("Attack", true);
+            animator.SetBool("isAround", false);
+        }
+        else
+        {
+            animator.SetBool("Attack", false);
             animator.SetBool("isAround", false);
         }
     }
